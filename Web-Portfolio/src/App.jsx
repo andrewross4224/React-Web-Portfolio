e8ba3a5c2afcc9ba6b394910676ea393effc5e27
Andrew Ross <andrew.ross4224@gmail.com>
2024-01-25 14:49:06 -0500

    did setup and testing of react-bootstrap

--- Web-Portfolio/src/App.jsx
+++ Web-Portfolio/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+
+import { Button } from 'react-bootstrap'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,15 +14,12 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className=''>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button style={{backgroundColor: 'var(--main)'}} onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
